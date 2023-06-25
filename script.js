a5b6275437011b1dc0831c2171c7a133570c80d7
Judy Zhang <101025113+JudyZhang05@users.noreply.github.com>
2023-06-25 15:09:56 -0400

    clickables got working subs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     document.getElementById("navbar").style.top = "0";
   }
 }
