4a41b9f296a02648059ada0b07363c2cc4b3b427
Judy Zhang <101025113+JudyZhang05@users.noreply.github.com>
2023-06-28 20:07:20 -0400

    altered code showcase

--- code port/code.js
+++ code port/code.js
@@ -26,3 +26,13 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-65px";
+  }
+}
