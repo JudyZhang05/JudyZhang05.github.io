6e98bab17f726bedbaa0cdebd92901e70a360fe4
Judy Zhang <101025113+JudyZhang05@users.noreply.github.com>
2023-09-02 14:03:51 -0400

    fixed contact page

--- contact/contact.js
+++ contact/contact.js
@@ -17,7 +17,7 @@ function aftersent(){
         emailmessage.style.textAlign = "left"
         emailmessage.value = ""
     }, 1500)
-    // alert("Apologies! The sending email function is still under construction")
+    // alert("Whoops! The email function is still under construction.")
 }
 
 function buttonSwitch(gone, come){
@@ -30,106 +30,132 @@ function buttonSwitch(gone, come){
     }
 }
 
-function sent(){
-// sendButton.addEventListener("click", () =>{
+sendButton.addEventListener("click", () =>{
     copytext()
     aftersent()
     buttonSwitch(goButton, sendButton)
-// })
+})
+
+goButton.addEventListener("click", () =>{
+    buttonSwitch(sendButton, goButton)
+})
+
+// switching displays
+const email = document.getElementById("emailChoice")
+const linkedin = document.getElementById("linkedinChoice")
+const github = document.getElementById("githubChoice")
+
+const contact = ["email","linkedin", "github"]
+
+function getElement(id){
+    return document.getElementById(id) 
 }
 
-function went(){
-// goButton.addEventListener("click", () =>{
-    buttonSwitch(sendButton, goButton)
-// })
+for (let choice of contact){
+    for (let sub = 0; sub < 2; sub++){
+        getElement(`${choice + 'Sub' + [sub+1]}`).addEventListener("click", () => {
+            alterDisplays(choice)
+        })
+    }
+}
+
+// function animatedSeq(){
+    
+// }
+
+function alterDisplays(section){
+    for (let each of contact){
+        getElement(`${each + 'Choice'}`).style.display = "none"
+    }
+    getElement(`${section + 'Choice'}`).style.display = "flex"
 }
 
 // "slide show >:V"
 
-var left = document.getElementById("reachOptLeft")
-var center = document.getElementById("reachOptCenter")
-var right = document.getElementById("reachOptRight")
+// var left = document.getElementById("reachOptLeft")
+// var center = document.getElementById("reachOptCenter")
+// var right = document.getElementById("reachOptRight")
 
 
 // LIST ORDER::     [0] github      [1] email     [1] linkedIn
 //INNER ORDER::     [0] Wrapper     [1] Sub       [1] Display Section
-const contact = {"github": ["choiceGithub","gitSub","githubSection"], "email": ["choiceEmail","emailSub","emailSection"], "linkedin": ["choiceLinkedIn","linkSub","linkedInSection"]}
+// const contact = {"github": ["choiceGithub","gitSub","githubSection"], "email": ["choiceEmail","emailSub","emailSection"], "linkedin": ["choiceLinkedIn","linkSub","linkedInSection"]}
 
-function getElement(id){
-    access = []
-    for (let tag = 0; tag < 3; tag++){
-        access.push(document.getElementById(id[tag]))
-    }
-    return access
-}
+// function getElement(id){
+//     access = []
+//     for (let tag = 0; tag < 3; tag++){
+//         access.push(document.getElementById(id[tag]))
+//     }
+//     return access
+// }
 
-const github = getElement(contact["github"])
-const email = getElement(contact["email"])
-const linkedin = getElement(contact["linkedin"])
+// const github = getElement(contact["github"])
+// const email = getElement(contact["email"])
+// const linkedin = getElement(contact["linkedin"])
 
-var identify = 0
-for (let tag in contact){
-    getElement(contact[tag])[1].value = identify
-    identify++
-    // console.log(`${contact[tag][1]} ${getElement(contact[tag])[1].value}`)
-}
+// var identify = 0
+// for (let tag in contact){
+//     getElement(contact[tag])[1].value = identify
+//     identify++
+//     ...HIDE => console.log(`${contact[tag][1]} ${getElement(contact[tag])[1].value}`)
+// }
 
-function swapContent(section, place){
-    const div = section
-    const papers = place
-    const clone = div.cloneNode(true)
+// function swapContent(section, place){
+//     const div = section
+//     const papers = place
+//     const clone = div.cloneNode(true)
 
-    while(papers.firstChild) papers.firstChild.remove()
+//     while(papers.firstChild) papers.firstChild.remove()
 
-    papers.appendChild(clone)
-}
+//     papers.appendChild(clone)
+// }
 
-function others(tagValue){
-    if (tagValue.value === 0){
-        // left.innerHTML = linkedin[1].innerHTML
-        // right.innerHTML = email[1].innerHTML
-        swapContent(linkedin[0], left)
-        swapContent(github[0], center)
-        swapContent(email[0], right)
-    } else if (tagValue === 1){
-        // left.innerHTML = github[1].innerHTML
-        // right.innerHTML = linkedin[1].innerHTML
-        swapContent(github[0], left)
-        swapContent(email[0], center)
-        swapContent(linkedin[0], right)
-    } else{
-        // left.innerHTML = email[1].innerHTML
-        // right.innerHTML = github[1].innerHTML
-        swapContent(email[0], left)
-        swapContent(linkedin[0], center)
-        swapContent(github[0], right)
-    }
-    if (email[1].value === 1){
-        console.log("woah")
-    }
-}
+// function others(tagValue){
+//     if (tagValue.value === 0){
+//         ...HIDE =>  left.innerHTML = linkedin[1].innerHTML
+//         ...HIDE =>  right.innerHTML = email[1].innerHTML
+//         swapContent(linkedin[0], left)
+//         swapContent(github[0], center)
+//         swapContent(email[0], right)
+//     } else if (tagValue === 1){
+//         ...HIDE => left.innerHTML = github[1].innerHTML
+//         ...HIDE =>  right.innerHTML = linkedin[1].innerHTML
+//         swapContent(github[0], left)
+//         swapContent(email[0], center)
+//         swapContent(linkedin[0], right)
+//     } else{
+//         ...HIDE =>  left.innerHTML = email[1].innerHTML
+//         ...HIDE =>  right.innerHTML = github[1].innerHTML
+//         swapContent(email[0], left)
+//         swapContent(linkedin[0], center)
+//         swapContent(github[0], right)
+//     }
+//     if (email[1].value === 1){
+//         console.log("woah")
+//     }
+// }
 
-function choice(tagValue){
-    for (let subs in contact){
-        var section = getElement(contact[subs]) 
-        if (section[1].value === tagValue.value){
-            section[1].style.display = "none"
-            section[2].style.display = "block"
-            // swapContent(section[0], center)
-        } else{
-            section[1].style.display = "block"
-            section[2].style.display = "none"
-            // others(tagValue)
-        }
-        others(tagValue)
-    }
-}
+// function choice(tagValue){
+//     for (let subs in contact){
+//         var section = getElement(contact[subs]) 
+//         if (section[1].value === tagValue.value){
+//             section[1].style.display = "none"
+//             section[2].style.display = "block"
+//             ...HIDE =>  swapContent(section[0], center)
+//         } else{
+//             section[1].style.display = "block"
+//             section[2].style.display = "none"
+//             ...HIDE =>  others(tagValue)
+//         }
+//         others(tagValue)
+//     }
+// }
 
-for (let each in contact){
-    getElement(contact[each])[1].addEventListener("click", () => {
-        choice(getElement(contact[each])[1])
-    })
-}
+// for (let each in contact){
+//     getElement(contact[each])[1].addEventListener("click", () => {
+//         choice(getElement(contact[each])[1])
+//     })
+// }
 
-console.log(email[1].value)
-choice(email[1])
+// console.log(email[1].value)
+// choice(email[1])
